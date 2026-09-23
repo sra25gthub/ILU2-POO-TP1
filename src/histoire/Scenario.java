0028dabc7ed9b4236ccc4743f431bf98550974f6
Sara Derkaoui <drs4756a@ensfsi.onmicrosoft.com>
2026-09-23 10:31:04 +0200

    premier commit

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -41,6 +41,8 @@ public class Scenario {
 //		System.out.println(etalFleur.acheterProduit(15, assurancetourix));
 //		System.out.println(village.partirVendeur(bonemine));
 //		System.out.println(village.afficherMarche());
+		
+		System.out.println("Hello");
 	}
 
 }
